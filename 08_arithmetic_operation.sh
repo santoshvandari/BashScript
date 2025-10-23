4f3f742bdf34b4ec27edcf3c3a799eeee3422169
Santosh Bhandari <santoshvandari1@gmail.com>
2025-10-23 19:24:54 +0545

    feat: condition in script

--- 08_arithmetic_operation.sh
+++ 08_arithmetic_operation.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# Variables Declaration
 x=10
 y=2
+
+# Multiplication
 let multiply=$x*$y
 let sum=$x+$y
 
 echo "Multiply : $multiply and Addition : $sum"
 
+# Calculation
 subtraction=$(($x-$y))
+division=$(($x/$y))
+modulas=$((11%2))
 
-echo "Subtraction : $subtraction"
+# Displaying in the Terminal
+echo -e "Subtraction : $subtraction\nDivision : $division\nModulas : $modulas"
 
 
 
---
+++ 09_if_else.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+read -p "Enter Your Age: " age
+
+if [[ $age -ge 18 ]]
+then
+    echo "You are Eligible to Vote"
+else
+    echo "You are not Eligible to Vote"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
