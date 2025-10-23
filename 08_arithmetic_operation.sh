95ff0e6fbeeb88507449f5850a5d3fbc7f043feb
Santosh Bhandari <santoshvandari1@gmail.com>
2025-10-23 19:04:11 +0545

    feat: arithmetic ops

---
+++ 08_arithmetic_operation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+x=10
+y=2
+let multiply=$x*$y
+let sum=$x+$y
+
+echo "Multiply : $multiply and Addition : $sum"
+
+subtraction=$(($x-$y))
+
+echo "Subtraction : $subtraction"
+
+
+
+
+
+
+
