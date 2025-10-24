acf46d1d4e0266c09903752c51f25e61b046a760
Santosh Bhandari <santoshvandari1@gmail.com>
2025-10-24 10:49:29 +0545

    feat: case

---
+++ 11_case.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Menu Options
+echo "Options
+a) Addition
+b) Difference
+c) Multiplication
+d) Division
+Enter Your Choice(a,b,c,d): "
+
+# User Choice
+read choice
+
+read -p "Enter first Number: " num1
+read -p "Enter Second Number: " num2
+
+# Case
+case $choice in
+    a) echo "Addition: $(($num1+$num2))";;
+    b) echo "Subtraction: $(($num1-$num2))";;
+    c) echo "Multiplicatiion: $(($num1*$num2))";;
+    d) echo "Division: $(($num1/$num2))";;
+    *) echo "Invalid Option Selection"
+
+
+esac
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
